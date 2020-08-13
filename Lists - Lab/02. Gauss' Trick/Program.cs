e81d951b42c8c86ffc42d59f2e2709e7d04c23d1
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:06:39 +0300

    Add files via upload

---
+++ Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                numbers[i] = numbers[i] + numbers[numbers.Count - 1 - i];
+                numbers[numbers.Count - 1 - i] = 0;
+            }
+            while (numbers.Contains(0))
+            {
+                numbers.Remove(0);
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
