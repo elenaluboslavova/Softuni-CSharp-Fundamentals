863e638d1257664aa51e50c0a8d77fa5f2d07208
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:06:27 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            float eps = 0.000001f;
+
+            if (Math.Abs(a - b) >= eps)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
