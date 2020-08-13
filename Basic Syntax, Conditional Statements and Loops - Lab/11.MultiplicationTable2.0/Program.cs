26e493e489212437c1600d67db75e7ed34246bac
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:46:36 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11.MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            if (k>10)
+            {
+                Console.WriteLine($"{n} X {k} = {n*k}");
+                Environment.Exit(0);
+            }
+            for (int i = k; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+            }
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/12.EvenNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12.EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while (n % 2 == 1)
+            {
+                Console.WriteLine("Please write an even number.");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("The number is: " + Math.Abs(n));
+
+
+        }
+    }
+}
