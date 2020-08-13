9e3a79a3e2bf1386bd6d356fe1b15b8de362cc75
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:28:55 +0300

    Add files via upload

---
+++ Methods - More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "int")
+            {
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(number * 2);
+            }
+            else if (type == "real")
+            {
+                double number = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{number * 1.5:F2}");
+            }
+            else if (type == "string")
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine($"${input}$");
+            }
+        }
+
+        
+    }
+}
---
+++ Methods - More Exercise/03. Longer Line/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _03._Longer_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double a1 = double.Parse(Console.ReadLine());
+            double b1 = double.Parse(Console.ReadLine());
+            double a2 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+
+            PrintLongerLine(x1, y1, x2, y2, a1, b1, a2, b2);
+        }
+
+        static void PrintLongerLine(double x1, double y1, double x2, double y2, double a1, double b1, double a2, double b2)
+        {
+            if ((Math.Abs(x1) + Math.Abs(y1) + Math.Abs(x2) + Math.Abs(y2)) < (Math.Abs(a1) + Math.Abs(b1) + Math.Abs(a2) + Math.Abs(b2)))
+            {
+                if ((Math.Abs(a1) + Math.Abs(b1)) > (Math.Abs(a2) + Math.Abs(b2)))
+                {
+                    Console.WriteLine($"({a2}, {b2})({a1}, {b1})");
+                }
+                else
+                {
+                    Console.WriteLine($"({a1}, {b1})({a2}, {b2})");
+                }
+            }
+            else
+            {
+                if ((Math.Abs(x1) + Math.Abs(y1)) > (Math.Abs(x2) + Math.Abs(y2)))
+                {
+                    Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+                }
+            }
+        }
+    }
+}
