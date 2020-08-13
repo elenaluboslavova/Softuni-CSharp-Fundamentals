a858f7fc1e40687d0081f073514169006d6a93c8
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:38:09 +0300

    Add files via upload

---
+++ Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            while (number != "END")
+            {
+                CheckIfPalindrome(number);
+                number = Console.ReadLine();
+            }
+            
+        }
+
+        static void CheckIfPalindrome(string number)
+        {
+            string reversed = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                reversed += number[i];
+            }
+            if (reversed == number)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+        }
+    }
+}
