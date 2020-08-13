3d269410ccd2628846ffa5e5efb660a608365bfc
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 16:19:02 +0300

    Add files via upload

---
+++ Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                string word = input[i];
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (!dictionary.ContainsKey(word[j]))
+                    {
+                        dictionary.Add(word[j], 1);
+                    }
+                    else
+                    {
+                        dictionary[word[j]]++;
+                    }
+                }
+            }
+
+            foreach(var pair in dictionary)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            while (command != "stop")
+            {
+                string resource = command;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(resource))
+                {
+                    dict.Add(resource, quantity);
+                }
+                else
+                {
+                    dict[resource] += quantity;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach(var pair in dict)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
