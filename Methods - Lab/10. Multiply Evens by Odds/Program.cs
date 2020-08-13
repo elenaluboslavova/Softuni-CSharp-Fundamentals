5cd57481417814ca714e88f68f4737d4681af6b9
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:47:09 +0300

    Add files via upload

---
+++ Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            n = Math.Abs(n);
+            Console.WriteLine(MultiplyEevenAndOddDigits(n));
+        }
+
+        static int CalculateOddDigitSum(int n, int isOdd)
+        {
+            string number = n.ToString();
+            int sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int currentDigit = int.Parse(number[i].ToString());
+                if (currentDigit % 2 == isOdd)
+                {
+                    sum += currentDigit;
+                }
+            }
+            return sum;
+        }
+        static int CalculateDigitSum(int n, int isOdd)
+        {
+            string number = n.ToString();
+            int sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int currentDigit = int.Parse(number[i].ToString());
+                if (currentDigit % 2 == isOdd)
+                {
+                    sum += currentDigit;
+                }
+            }
+            return sum;
+        }
+        static int MultiplyEevenAndOddDigits(int n)
+        {
+            return CalculateDigitSum(n, 0) * CalculateOddDigitSum(n, 1);
+
+        }
+    }
+}
