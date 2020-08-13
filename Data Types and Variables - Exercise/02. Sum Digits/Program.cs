48b5328aeafbe18e56b1de192f4a407cfeca0256
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:11:40 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int n1 = number;
+
+            do
+            {
+                sum += n1 % 10;
+                n1 = n1 / 10;
+            }
+            while (n1 > 0);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Ceiling(people*1.0/capacity));
+        }
+    }
+}
