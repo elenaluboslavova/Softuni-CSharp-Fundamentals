1a424a7810a9ae18c524c4bca643af37a509c078
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:39:02 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09.PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double sabrePrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+
+            double sum = sabrePrice * Math.Ceiling(students + students * 0.1) + robePrice * students + beltPrice * (students - students / 6);
+
+            if (money >= sum)
+            {
+                Console.WriteLine($"The money is enough - it would cost {sum:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {sum-money:F2}lv more.");
+            }
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/10.RageExpenses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10.RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int trashedHeadsets = lostGames / 2;
+            int trashedMouses = lostGames / 3;
+            int trashedKeyboards = lostGames / 6;
+            int trashedDisplays = trashedKeyboards / 2;
+
+            double expenses = headsetPrice * trashedHeadsets + mousePrice * trashedMouses
+                + keyboardPrice * trashedKeyboards + displayPrice * trashedDisplays;
+
+            Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
+        }
+    }
+}
