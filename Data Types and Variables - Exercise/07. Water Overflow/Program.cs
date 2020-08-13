99a8224929360d124d7caf21aac9e77c0c82b09d
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:14:59 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int capacity = 255;
+
+            for (int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                capacity -= liters;
+
+                if (capacity < 0)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    capacity += liters;
+                    continue;
+                }
+            }
+            Console.WriteLine(255-capacity);
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string model = "";
+            double radius = 0;
+            int height = 0;
+            double volume = 0;
+
+            double maxVolume = double.MinValue;
+            string maxModel = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                model = Console.ReadLine();
+                radius = double.Parse(Console.ReadLine());
+                height = int.Parse(Console.ReadLine());
+
+                volume = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (volume >= maxVolume)
+                {
+                    maxVolume = volume;
+                    maxModel = model;
+                }
+
+            }
+            Console.WriteLine(maxModel);
+        }
+    }
+}
