64d09a1ef2b742db142d6f9d405b99274461837d
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:43:19 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _09.SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int k = 0;
+            int a = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                Console.WriteLine(i+k);
+                k++;
+                a = i + k;
+                sum += a;
+            }
+            Console.WriteLine($"Sum: {sum-n}");
+        }
+    }
+}
