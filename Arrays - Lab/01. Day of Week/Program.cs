1bb78fab7e2ddd9653493b8ff1364ca044df9985
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:00:06 +0300

    Add files via upload

---
+++ Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if (number > 7 || number <= 0)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[number-1]);
+            }
+        }
+    }
+}
