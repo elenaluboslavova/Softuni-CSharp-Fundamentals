3e76dc49902bfd1a65c351da9224252c73570d7b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 15:59:10 +0300

    Add files via upload

---
+++ Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Regex datesRegex = new Regex(@"[0-9]{2}(.)[A-Za-z]{3}\1\d{4}");
+            MatchCollection dates = Regex.Matches(Console.ReadLine(), @"(?<day>[0-9]{2})(?<separator>.)(?<month>[A-Za-z]{3})\k<separator>(\d{4})");
+
+            foreach (Match date in dates)
+            {
+                Console.WriteLine($"Day: {date.Groups["day"]}, Month: {date.Groups["month"]}, Year: {date.Groups[1]}");
+            }
+        }
+    }
+}
---
+++ Regular Expressions - Lab/Regular Expressions - Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regular_Expressions___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
