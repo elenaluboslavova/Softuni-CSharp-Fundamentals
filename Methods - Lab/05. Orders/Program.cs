00ae2b73899f8ea3fc3e118e9678fa94a9b2cfad
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:44:38 +0300

    Add files via upload

---
+++ Methods - Lab/05. Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            PrintPrice(product, quantity);
+        }
+
+        static void PrintPrice(string product, int quantity)
+        {
+            double sum = 0;
+            switch (product)
+            {
+                case "coffee":
+                    sum = quantity * 1.50;
+                    break;
+                case "water":
+                    sum = quantity * 1.00;
+                    break;
+                case "coke":
+                    sum = quantity * 1.40;
+                    break;
+                case "snacks":
+                    sum = quantity * 2.00;
+                    break;
+            }
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
