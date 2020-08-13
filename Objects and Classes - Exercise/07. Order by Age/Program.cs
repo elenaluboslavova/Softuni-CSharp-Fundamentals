7391c388c503bce365743a99ddcfe2377f402419
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 16:46:33 +0300

    Add files via upload

---
+++ Objects and Classes - Exercise/07. Order by Age/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _07._Order_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Person> people = new List<Person>();
+
+            while (input != "End")
+            {
+                string[] data = input.Split().ToArray();
+
+                Person person = new Person();
+
+                person.Name = data[0];
+                person.ID = int.Parse(data[1]);
+                person.Age = int.Parse(data[2]);
+
+                
+                people.Add(person);
+
+                input = Console.ReadLine();
+            }
+            foreach (var person in people.OrderBy(person => person.Age))
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+            }
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ Objects and Classes - Exercise/Objects and Classes - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objects_and_Classes___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
