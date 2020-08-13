9d811717753e165101d7150a8edba251c84496bc
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:20:46 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal number = 0;
+            decimal sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
