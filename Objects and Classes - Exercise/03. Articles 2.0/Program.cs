01d7f4846d4758a8b91f333f48b8ca7344484988
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 16:44:20 +0300

    Add files via upload

---
+++ Objects and Classes - Exercise/03. Articles 2.0/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Articles_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countArticles = int.Parse(Console.ReadLine());
+
+            List<Article> articles = new List<Article>();
+
+            string[] input;
+
+            for (int i = 0; i < countArticles; i++)
+            {
+                input = Console.ReadLine().Split(", ");
+                var article = new Article(input[0], input[1], input[2]);
+                articles.Add(article);
+            }
+
+            string orderByWhat = Console.ReadLine();
+
+            switch (orderByWhat)
+            {
+                case "title":
+                    articles = articles.OrderBy(x => x.Title).ToList();
+                    break;
+                case "content":
+                    articles = articles.OrderBy(x => x.Content).ToList();
+                    break;
+                case "author":
+                    articles = articles.OrderBy(x => x.Author).ToList();
+                    break;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, articles));
+        }
+    }
+
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ Objects and Classes - Exercise/04. Students/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                var studentInfo = Console.ReadLine().Split().ToArray();
+
+                Student student = new Student(studentInfo[0], studentInfo[1], double.Parse(studentInfo[2]));
+
+                students.Add(student);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, students.OrderByDescending(x => x.Grade)));
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string firstName, string lastName, double grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:F2}";
+        }
+    }
+}
