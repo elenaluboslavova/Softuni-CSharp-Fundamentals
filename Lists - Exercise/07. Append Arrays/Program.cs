a64c1ce4c8675faf2a73e13ee6027248daf3b9b5
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:01:16 +0300

    Add files via upload

---
+++ Lists - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split('|').Reverse().ToList();
+
+            List<int> numbers = new List<int>();
+
+            foreach (var item in list)
+            {
+                numbers.AddRange(item.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList());
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
