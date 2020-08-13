0921395859328c0e0787c34da26fcbe1dd7a4f3b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:03:15 +0300

    Add files via upload

---
+++ Lists - Exercise/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int sum = 0;
+            while (list.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+                if (index < 0)
+                {
+                    int first = list[0];
+                    list.RemoveAt(0);
+                    list.Insert(0, list[list.Count - 1]);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] > first)
+                        {
+                            list[i] -= first;
+                        }
+                        else if (list[i] <= first)
+                        {
+                            list[i] += first;
+                        }
+                        
+                    }
+                    sum += first;
+                    continue;
+                }
+                if (index >= list.Count)
+                {
+                    int last = list[list.Count - 1];
+                    list.RemoveAt(list.Count - 1);
+                    list.Add(list[0]);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] > last)
+                        {
+                            list[i] -= last;
+                        }
+                        else if (list[i] <= last)
+                        {
+                            list[i] += last;
+                        }
+                        
+                    }
+                    sum += last;
+                    continue;
+                }
+
+                int number = list[index];
+                list.RemoveAt(index);
+                sum += number;
+                
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] > number)
+                    {
+                        list[i] -= number;
+                    }
+                    else if (list[i] <= number)
+                    {
+                        list[i] += number;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Lists - Exercise/10. SoftUni Course Planning/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _10._SoftUni_Course_Planning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(", ").ToList();
+
+            string command = string.Empty;
+            string action = string.Empty;
+            string element = string.Empty;
+            string lesson = string.Empty;
+            int index = 0;
+            string element1 = string.Empty;
+            string firstLesson = string.Empty;
+            string secondLesson = string.Empty;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "course start")
+                {
+                    break;
+                }
+
+                List<string> incomingDetails = command.Split(":").ToList();
+
+                for (int i = 0; i < incomingDetails.Count; i++)
+                {
+                    action = incomingDetails[0];
+                    element = incomingDetails[1];
+
+                    if (action == "Insert")
+                    {
+                        index = int.Parse(incomingDetails[2]);
+                        lesson = element;
+                        break;
+                    }
+                    else if (action == "Swap")
+                    {
+                        string elementInsert = incomingDetails[2];
+                        firstLesson = incomingDetails[1];
+                        secondLesson = incomingDetails[2];
+                        break;
+                    }
+                }
+                for (int i = 0; i < input.Count; i++)
+                {
+                    if (action == "Add")
+                    {
+                        if (!input.Contains(element))
+                        {
+                            input.Add(element);
+                        }
+                    }
+                    else if (action == "Remove")
+                    {
+                        string exercise = element + '-' + "Exercise";
+                        if (input.Contains(element))
+                        {
+                            input.Remove(element);
+                        }
+                        if (input.Contains(exercise))
+                        {
+                            input.Remove(exercise);
+                        }
+                    }
+                    else if (action == "Exercise")
+                    {
+                        string exercise = element + '-' + "Exercise";
+                        if (!input.Contains(element))
+                        {
+                            if (!input.Contains(exercise))
+                            {
+                                input.Add(element);
+                                input.Add(exercise);
+                                break;
+                            }
+                            break;
+                        }
+                        else if (input.Contains(element) && !input.Contains(exercise))
+                        {
+                            int indexx = input.IndexOf(element);
+                            input.Insert(indexx + 1, exercise);
+                            break;
+                        }
+                    }
+                    else if (action == "Insert")
+                    {
+                        if (!input.Contains(lesson))
+                        {
+                            input.Insert(index, lesson);
+                            break;
+                        }
+                        break;
+                    }
+                    else if (action == "Swap")
+                    {
+                        if (input.Contains(firstLesson) && input.Contains(secondLesson))
+                        {
+                            string exercise = firstLesson + '-' + "Exercise";
+                            int index1 = input.IndexOf(firstLesson);
+                            int index2 = input.IndexOf(secondLesson);
+                            string temp = firstLesson;
+                            input.RemoveAt(index1);
+                            input.Insert(index1, secondLesson);
+
+                            if (input.Contains(exercise))
+                            {
+                                input.Remove(exercise);
+                                input.Insert(index1 + 1, exercise);
+                            }
+
+                            exercise = string.Empty;
+                            exercise = secondLesson + '-' + "Exercise";
+                            input.RemoveAt(index2);
+                            input.Insert(index2, temp);
+
+                            if (input.Contains(exercise))
+                            {
+                                input.Remove(exercise);
+                                input.Insert(index1 + 1, exercise);
+                            }
+                        }
+                        break;
+                    }
+                }
+                
+            }
+            for (int i = 0; i < input.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{input[i]}");
+            }
+        }
+    }
+}
