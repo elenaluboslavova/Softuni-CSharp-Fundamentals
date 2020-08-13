80e0c230263cf40eaf9b00fad1713dc84c8108d3
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 18:26:34 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int digit = 0;
+            bool isSpecialNum = false;
+            for (int i = 1; i <= n; i++)
+            {
+                digit = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", digit, isSpecialNum);
+                sum = 0;
+                i = digit;
+            }
+
+        }
+    }
+}
