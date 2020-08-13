598d0a99dc602a05d32d22253532d7c3a225e7aa
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 17:53:30 +0300

    Add files via upload

---
+++ Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < array.Length-1; i++)
+            {
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+
+                    if (array[i] + array[j] == number)
+                    {
+                        Console.Write($"{array[i]} {array[j]}");
+                        Console.WriteLine();
+                    }
+
+                }
+            }
+        }
+    }
+}
