18ab3bd3aad326a2def7e80a688b54d8058caf62
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 16:34:08 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int[] sorted = numbers.OrderByDescending(n => n).ToArray();
+
+            if (sorted.Length <= 3)
+            {
+                Console.WriteLine(string.Join(" ", sorted));
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(sorted[i] + " ");
+                }
+            }
+            
+        }
+    }
+}
