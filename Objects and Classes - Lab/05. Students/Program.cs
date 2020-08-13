41a0adb9ca1da805ff03d2c6e0976b71bd43dbfa
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 16:52:19 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/05. Students/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string line = Console.ReadLine();
+            while (line != "end")
+            {
+                string[] tokens = line.Split();
+
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                string hometown = tokens[3];
+
+                Student student = new Student()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    Hometown = hometown
+                };
+
+                students.Add(student);
+
+                line = Console.ReadLine();
+            }
+
+            string filterCity = Console.ReadLine();
+
+            List<Student> filteredStudents = students
+                .Where(s => s.Hometown == filterCity).ToList();
+
+            foreach(Student student in filteredStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Hometown { get; set; }
+    }
+}
---
+++ Objects and Classes - Lab/07. Store Boxes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Store_Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> data = new List<Box>();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split();
+
+                string serialNumber = tokens[0];
+                string itemName = tokens[1];
+                int itemQuantity = int.Parse(tokens[2]);
+                decimal itemPrice = int.Parse(tokens[3]);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+    class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+    class Box
+    {
+        public Box()
+        {
+            Item = new Item();
+        }
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal PriceBox { get; set; }
+    }
+}
