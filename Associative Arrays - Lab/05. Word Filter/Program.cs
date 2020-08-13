0c86efd22576b663d6051796ad9f3ddc6a5ad2db
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 16:35:56 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/05. Word Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().Where(w => w.Length % 2 == 0).ToList();
+
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
