90b7f88f17505981847510464bf8eaef41e102e5
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-08-13 16:07:21 +0300

    Add files via upload

---
+++ Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToList();
+
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                char current = input[i];
+                char next = input[i + 1];
+                if (current == next)
+                {
+                    input.RemoveAt(i);
+                    i--;
+                }
+            }
+            Console.WriteLine(string.Join("", input));
+        }
+    }
+}
